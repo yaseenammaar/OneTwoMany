11f964532adefda142f634e1eebff3676c94d721
Yaseen Ammaar <47599553+yaseenammaar@users.noreply.github.com>
2022-06-23 01:25:37 +0530

    Create OneTwoMany_Multiround.sol

---
+++ OneTwoMany_Multiround.sol
@@ -0,0 +1,15 @@
+// SPDX-License-Identifier: MIT
+
+pragma solidity ^0.9.0;
+import "./OneTwoMany.sol";
+
+contract OneTwoMany_Multiround {
+
+    function createMultipleRooms() public {
+
+        //Room Code
+
+    }
+    
+
+}
