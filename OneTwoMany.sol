56141b893c93abceca3cdc67d4d9acb2f27b3b85
Yaseen Ammaar <47599553+yaseenammaar@users.noreply.github.com>
2022-06-23 01:24:28 +0530

    Create OneTwoMany.sol

---
+++ OneTwoMany.sol
@@ -0,0 +1,31 @@
+// SPDX-License-Identifier: MIT
+
+pragma solidity ^0.9.0;
+
+contract OneTwoMany {
+
+    function createRoom() public {
+
+        //Room Code
+
+    }
+
+    function stakeAsset() public {
+
+        //Staking Code
+
+    }
+
+    function declareWinner() public {
+
+        //Winner Code
+
+    }
+
+    function transferAssets() public {
+
+        //Winner Code
+
+    }
+
+}
